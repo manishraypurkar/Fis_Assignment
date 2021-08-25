1bf484a702392ec3aa3767d14ab4b3de45c3f7a1
Manish raypurkar <35144263+manishraypurkar@users.noreply.github.com>
2021-08-25 19:28:49 +0530

    Add files via upload

---
+++ MenuApp/MenuApp/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+ Author -manish anil raypurkar
+ Date -23/08/21
+Description - Menu app for calculator using switch case
+ */
+namespace MenuApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int choice;
+            Console.WriteLine("1.add\n2.sub\n3.multiply\n4.divide\n5.exit");
+            Console.WriteLine("Enter your choice");
+            choice = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the values of x and y");
+            int x=Convert.ToInt32(Console.ReadLine());
+            int y=Convert.ToInt32(Console.ReadLine());
+            int r;
+
+            switch(choice)
+            {
+                case 1: r = x + y;
+                    Console.WriteLine("Addition is "+ r);
+                    break;
+                case 2: r = x - y;
+                    Console.WriteLine("Substraction is "+r);
+                    break;
+                case 3:
+                    r = x * y;
+                    Console.WriteLine("Multipication is "+ r);
+                    break;
+                case 4:
+                    r = x/y;
+                    Console.WriteLine("Dividation/Quotient is "+ r);
+                    break;
+                case 5:
+                    Console.WriteLine("exiting..");
+                    Environment.Exit(0);
+                    break;
+                default:
+                    Console.WriteLine("invalid choice");
+                    break;
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ TransposeMatrix/TransposeMatrix/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ Author -manish anil raypurkar
+ Date -23/08/21
+Description - Accepts 2*2 matrix from user and display transpose matrix.
+ */
+namespace TransposeMatrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] matrix = new int[2,2];
+            int[,] tmatrix = new int[2, 2];
+
+
+            Console.WriteLine("Enter the 2*2 matrix");
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for(int j=0;j<matrix.GetLength(1);j++)
+                {
+                    matrix[i,j] = Convert.ToInt32(Console.ReadLine());
+
+                }
+            }
+
+            for(int i=0;i<tmatrix.GetLength(0);i++)
+            {
+                for(int j=0;j<tmatrix.GetLength(1); j++)
+                {
+                    tmatrix[i, j] = matrix[j,i];
+                }
+            }
+            Console.WriteLine("Transpose Matrix is");
+
+            for (int i = 0; i < tmatrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < tmatrix.GetLength(1); j++)
+                {
+                   Console.Write(tmatrix[i, j]);
+                    Console.Write("\t");
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
